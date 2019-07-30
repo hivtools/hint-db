7122e93692e58b08f7cfb6db30a429ea0600f454
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-07-30 15:11:13 +0100

    typo

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # Do not run if this is a pull request build - don't want to push to master yet
-if [ $TRAVIS_PULL_REQUEST == "false" ]; then
+if [ $TRAVIS_PULL_REQUEST != "false" ]; then
    exit 0
 fi
 
