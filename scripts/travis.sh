8601f0063b3fec1f6b563da41ed2eac474f78c00
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-07-30 12:18:28 +0100

    Take out env var check

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 set -ex
 
-if [["$DOCKER_PASSWORD" != ""]]
-then
-  echo $DOCKER_PASSWORD | \
+echo $DOCKER_PASSWORD | \
     docker login -u $DOCKER_USERNAME --password-stdin
-fi
+
 
 ./scripts/build
 ./scripts/push
