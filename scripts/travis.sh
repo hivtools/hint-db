bd8331e2927663fea5fef549e0946f10d894e5e5
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-07-30 12:15:40 +0100

    push images in travis script

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,1 +1,15 @@
-echo testing
+#!/usr/bin/env bash
+set -ex
+
+if [["$DOCKER_PASSWORD" != ""]]
+then
+  echo $DOCKER_PASSWORD | \
+    docker login -u $DOCKER_USERNAME --password-stdin
+fi
+
+./scripts/build
+./scripts/push
+
+cd migrations
+./scripts/build
+./scripts/push
