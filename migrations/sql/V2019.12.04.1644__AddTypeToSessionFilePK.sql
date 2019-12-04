44ab988d9f1b67e3ac0239c5ae2a0ada9b03a9ed
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-12-04 17:05:56 +0000

    Try that again...

--- migrations/sql/V2019.12.04.1644__AddTypeToSessionFilePK.sql
+++ migrations/sql/V2019.12.04.1644__AddTypeToSessionFilePK.sql
@@ -1,6 +1,6 @@
 ALTER TABLE session_file
-  DROP CONSTRAINT session_file_pk;
+  DROP CONSTRAINT session_file_pkey;
 
 ALTER TABLE session_file
-  ADD CONSTRAINT session_file_pk
+  ADD CONSTRAINT session_file_pkey
     PRIMARY KEY (session, hash, type);
