80eec8c86328085f3a93ec9fcb57dd371aaf6b1a
Emma Russell <EmmaRussellKilburn@gmail.com>
2020-06-25 11:06:45 +0100

    Added version and additional columns to user session

---
+++ migrations/sql/V2020.06.25.1104__AddVersion.sql
@@ -0,0 +1,15 @@
+CREATE TABLE version
+(
+    id SERIAL,
+    user_id TEXT references users (id),
+    name TEXT NOT NULL,
+    note TEXT
+);
+
+ALTER TABLE user_session
+ADD COLUMN version_id INTEGER references version (id),
+ADD COLUMN state TEXT,
+ADD COLUMN note TEXT,
+ADD COLUMN created TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() at time zone 'utc'),
+ADD COLUMN updated TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() at time zone 'utc'),
+ADD COLUMN deleted BOOLEAN DEFAULT FALSE;
